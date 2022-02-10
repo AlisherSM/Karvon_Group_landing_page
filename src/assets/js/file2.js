20313d5f463117b323eebd6d736c78f56b101631
AlisherSM <Salixov.alisher@gmail.com>
2022-02-10 14:35:46 +0500

    10.02

--- src/assets/js/file2.js
+++ src/assets/js/file2.js
@@ -1,1 +1,35 @@
 console.log('file 2');
+
+
+import "slick-carousel";
+import $ from "jquery";
+
+// Partners swiper "https://kenwheeler.github.io/slick/"
+  
+$(".partners__slider").slick ({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        draggable: true,
+        
+        
+});
+$(".partners__slider2").slick ({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        draggable: true,
+        rtl: true
+        
+        
+        
+});
+
+
+ 
+
+// asad
