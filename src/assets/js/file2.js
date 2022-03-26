7f1c4ec11dbc005db2208389c2ae65107fd3ced8
AlisherSM <Salixov.alisher@gmail.com>
2022-03-26 23:44:28 +0500

    26.03

--- src/assets/js/file2.js
+++ src/assets/js/file2.js
@@ -2,113 +2,135 @@ console.log('file 2');
 import "slick-carousel";
 import $ from "jquery";
 // Partners swiper "https://kenwheeler.github.io/slick/" 
-const partners = document.querySelector('.partners');
-const partnersObserver = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add ("show");
-    } else {
-      entry.target.classList.remove ("show");
-    }
+var currentPage = window.location.pathname;
+if (currentPage == "/") {
+  const activePage = document.querySelectorAll('.header__pagesin--home')
+  activePage.forEach((el) => {
+    el.classList.add("active");
   })
-})
-
-partnersObserver.observe(partners);
-
-
-
-$(".partners__slider").slick ({
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        arrows: false,
-        pauseOnHover: false,
-        responsive: [
-                {
-                  breakpoint: 991,
-                  settings: {
-                    slidesToShow: 4,
-                  }
-                },
-                {
-                  breakpoint: 600,
-                  settings: {
-                    slidesToShow: 3,
-                  }
-                },            
-              ] 
-});
-$(".partners__slider2").slick ({
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        arrows: false,
-        
-        rtl: true,
-        pauseOnHover: false,
-        
-        responsive: [
-                {
-                  breakpoint: 991,
-                  settings: {
-                    slidesToShow: 4,
-                  }
-                },
-                {
-                  breakpoint: 600,
-                  settings: {
-                    slidesToShow: 3,
-                  }
-                },            
-              ] 
-});
-
-const animations = document.querySelectorAll('.animation');
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add ('animate')
-    }
-  })
-})
-animations.forEach(animation => {
-  observer.observe(animation)
-})
-
-const time = 1000;
-const step = 1;
-
-
-function countNum (num, elem) {
   
-  let l = document.querySelector('#' + elem);
-  let n = 0;
-  let t = Math.round (time/(num/step));
-  let interval = setInterval (() => {
-    n = n + step;
-    if (n == num) {
-      clearInterval(interval);
-      
-    }
-    l.innerHTML = n;
-  },
-    t);
-}
-const nums = document.querySelectorAll ('.number');
-const observerNum = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      countNum (30, 'num-1');
-      countNum (15, 'num-2');
-      countNum (5, 'num-3');
-    }
+    
+  const partners = document.querySelector('.partners');
+  const partnersObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add ("show");
+      } else {
+        entry.target.classList.remove ("show");
+      }
+    })
   })
-})
-nums.forEach(num => {
-  observerNum.observe(num)
-})
+
+  partnersObserver.observe(partners);
+
+
+
+  $(".partners__slider").slick ({
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 1500,
+          arrows: false,
+          pauseOnHover: false,
+          responsive: [
+                  {
+                    breakpoint: 991,
+                    settings: {
+                      slidesToShow: 4,
+                    }
+                  },
+                  {
+                    breakpoint: 600,
+                    settings: {
+                      slidesToShow: 3,
+                    }
+                  },            
+                ] 
+  });
+  $(".partners__slider2").slick ({
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 1500,
+          arrows: false,
+          
+          rtl: true,
+          pauseOnHover: false,
+          
+          responsive: [
+                  {
+                    breakpoint: 991,
+                    settings: {
+                      slidesToShow: 4,
+                    }
+                  },
+                  {
+                    breakpoint: 600,
+                    settings: {
+                      slidesToShow: 3,
+                    }
+                  },            
+                ] 
+  });
+
+  const animations = document.querySelectorAll('.animation');
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add ('animate')
+      }
+    })
+  })
+  animations.forEach(animation => {
+    observer.observe(animation)
+  })
+
+  const time = 1000;
+  const step = 1;
+
+
+  function countNum (num, elem) {
+    
+    let l = document.querySelector('#' + elem);
+    let n = 0;
+    let t = Math.round (time/(num/step));
+    let interval = setInterval (() => {
+      n = n + step;
+      if (n == num) {
+        clearInterval(interval);
+        
+      }
+      l.innerHTML = n;
+    },
+      t);
+  }
+  const nums = document.querySelectorAll ('.number');
+  const observerNum = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        countNum (30, 'num-1');
+        countNum (15, 'num-2');
+        countNum (5, 'num-3');
+      }
+    })
+  })
+  nums.forEach(num => {
+    observerNum.observe(num)
+  })
+}
+
+
+if (currentPage == "/about.html") {
+  const activePage = document.querySelectorAll('.header__pagesin--about')
+  activePage.forEach ((el) => {
+    el.classList.add("active")
+  })
+}
+
+
+
+
+
 
 // MODAL
 var modalContacts = document.getElementsByClassName('modal__contacts')[0];
